5bf53b456f9ebb8fcd1e7d7d89cbe0d4c3609cc8
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-05-12 22:55:14 -0500

    got data on the page switching from the dropdown

--- js/wilson.js
+++ js/wilson.js
@@ -1,5 +1,7 @@
 //full data 
 d3.csv("data/data.csv").then(function(data){
+    var xLabel = "In Poverty (%)";
+    var yLabel = "Lacks Healthcare (%)";
 
     //Plot the data!!!!
     var margin = {top: 10, right: 30, bottom: 30, left: 60},
@@ -29,118 +31,151 @@ d3.csv("data/data.csv").then(function(data){
     svg.append("g")
         .call(d3.axisLeft(y));
 
-    var xAxis = function setX(d) { return x(d.poverty); }
-    var yAxis = function setY(d) { return y(d.healthcare); }
+    //create a dropdown menu for the other 2 plots
+    var opt = ["Age VS Smokers", "Poverty Vs Healthcare", "Income VS Obesity"];
+    var dropDown = d3.select("#btn").append('select');
+    
+    dropDown
+    .selectAll('myOptions')
+    .data(opt)
+    .enter()
+    .append('option')
+    .text(function (d) { return d;})
+    .attr("value", function (d) { return d;})
 
-    //GET THE DATA
-    draw(data);
-    function draw(data){
-        svg.append('g')
-        .selectAll("circle")
+    //starter plot
+    svg.append('g')
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+        .attr("cx", function (d) { return x(d.age); })
+        .attr("cy", function (d) { return y(d.smokes); })
+        .attr("r", 10)
+        .attr("opacity", ".8")
+        .style("fill", "#565051")
+
+    svg.append('g')
+        .selectAll("text")
         .data(data)
         .enter()
-        .append("circle")
-            .attr("cx", xAxis)
-            .attr("cy",yAxis)
-            .attr("r", 10)
-            .attr("opacity", ".8")
-            .style("fill", "#565051")
+        .append("text")
+            .attr("x", function (d) { return x(d.age-.25); })
+            .attr("y", function (d) { return y(d.smokes-.15); })
+            .text(function(d){ return d.abbr})
+            .attr("font-size", "9px")
+            .attr("fill", "white");
 
-        svg.append('g')
+    //update the plot
+    function updatePlot(choice){
+        if (choice === "Age VS Smokers"){
+            svg.append('g')
+            .selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+                .attr("cx", function (d) { return x(d.age); })
+                .attr("cy", function (d) { return y(d.smokes); })
+                .attr("r", 10)
+                .attr("opacity", ".8")
+                .style("fill", "#565051")
+
+            svg.append('g')
             .selectAll("text")
             .data(data)
             .enter()
             .append("text")
-                .attr("x",xAxis)
-                .attr("y", yAxis)
+                .attr("x", function (d) { return x(d.age-.25); })
+                .attr("y", function (d) { return y(d.smokes-.15); })
                 .text(function(d){ return d.abbr})
                 .attr("font-size", "9px")
                 .attr("fill", "white");
+
+            xLabel = "Age (Median)";
+            yLabel = "Smokers (%)";
+        }
+        else if(choice === "Poverty Vs Healthcare"){
+            svg.append('g')
+                .selectAll("circle")
+                .data(data)
+                .enter()
+                .append("circle")
+                    .attr("cx", function (d) { return x(d.poverty); })
+                    .attr("cy", function (d) { return y(d.healthcare); })
+                    .attr("r", 10)
+                    .attr("opacity", ".8")
+                    .style("fill", "#565051")
+
+            svg.append('g')
+                .selectAll("text")
+                .data(data)
+                .enter()
+                .append("text")
+                    .attr("x", function (d) { return x(d.poverty-.25); })
+                    .attr("y", function (d) { return y(d.healthcare-.15); })
+                    .text(function(d){ return d.abbr})
+                    .attr("font-size", "9px")
+                    .attr("fill", "white");
+
+            xLabel = "In Poverty (%)";
+            yLabel = "Lacks Healthcare (%)";
+        }
+        else if(choice === "Income VS Obesity"){
+                svg.append('g')
+                .selectAll("circle")
+                .data(data)
+                .enter()
+                .append("circle")
+                    .attr("cx", function (d) { return x(d.income); })
+                    .attr("cy", function (d) { return y(d.obesity); })
+                    .attr("r", 10)
+                    .attr("opacity", ".8")
+                    .style("fill", "#565051")
+
+            svg.append('g')
+                .selectAll("text")
+                .data(data)
+                .enter()
+                .append("text")
+                    .attr("x", function (d) { return x(d.income-.25); })
+                    .attr("y", function (d) { return y(d.obesity-.15); })
+                    .text(function(d){ return d.abbr})
+                    .attr("font-size", "9px")
+                    .attr("fill", "white");
+
+            xLabel = "Income (Median)";
+            yLabel = "Obesity (%)";
+        }
+        // Add the x Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        svg.append("text")             
+            .attr("transform",
+                "translate(" + (width/2) + " ," + (height + margin.top + 18) + ")")
+            .style("text-anchor", "middle")
+            .text(xLabel);
+
+        // Add the y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x",0 - (height / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text(yLabel);
     }
-    // Add the x Axis
-    svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
 
-    svg.append("text")             
-    .attr("transform",
-        "translate(" + (width/2) + " ," + (height + margin.top + 18) + ")")
-    .style("text-anchor", "middle")
-    //on Click to show data
-    .on("click",function(){
-        xAxis = function setX(d) { return x(d.poverty); };
-        draw(data);
-        console.log("click");
+    //dropdown on change
+    dropDown.on("change",function(d) {
+        var selectedOption = d3.select(this).property("value")
+        
+        //call the update function
+        updatePlot(selectedOption)
     })
-    .text("In Poverty (%)");
 
-    svg.append("text")             
-    .attr("transform",
-        "translate(" + (width/6) + " ," + (height + margin.top + 18) + ")")
-    .style("text-anchor", "middle")
-    //on Click to show data
-    .on("click",function(){
-        xAxis = function setX(d) { return x(d.age); };
-        draw(data);
-        console.log("click");
-    })
-    .text("Age (Median)");
-
-    svg.append("text")             
-    .attr("transform",
-        "translate(" + (width/1.2) + " ," + (height + margin.top + 18) + ")")
-    .style("text-anchor", "middle")
-    //on Click to show data
-    .on("click",function(){
-        xAxis = function setX(d) { return x(d.income); };
-        draw(data);
-        console.log("click");
-    })
-    .text("Household Income (Median)");
-    
-    // Add the y Axis
-    svg.append("g")
-    .call(d3.axisLeft(y));
-
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x",0 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        //on Click to show data
-        .on("click",function(){
-            yAxis = function setY(d) { return y(d.healthcare); };
-            draw(data);
-            console.log("click");
-        })
-        .text("Lacks Healthcare (%)");
-
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x",200 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        //on Click to show data
-        .on("click",function(){
-            yAxis = function setY(d) { return y(d.obesity); };
-            draw(data);
-            console.log("click");
-        })
-        .text("Obese (%)");
-    
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x",-200 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        //on Click to show data
-        .on("click",function(){
-            yAxis = function setY(d) { return y(d.smokes); };
-            draw(data);
-            console.log("click");
-        })
-        .text("Smokes (%)");
 })
